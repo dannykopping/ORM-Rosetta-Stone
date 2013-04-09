24e7df673e54a8523a24852e7da54c49594692cd
dannykopping <dannykopping@gmail.com>
2013-04-09 23:04:12 +0200

    Moved library files to "lib" folder

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -1,31 +1,29 @@
 <?php
-
-
-use Monolog\Handler\StreamHandler;
-use Monolog\Logger;
+    use Monolog\Handler\StreamHandler;
+    use Monolog\Logger;
     use ORMTest\Base\Registry;
 
     abstract class BaseTest extends PHPUnit_Framework_TestCase
-{
-    /**
-     * @var     Logger
-     */
-    protected $log;
-
-    protected function setUp()
     {
-        $this->log = new Logger('orm-log');
-        $this->log->pushHandler(new StreamHandler(__DIR__ . '/../log.txt'));
+        /**
+         * @var     Logger
+         */
+        protected $log;
 
-        new ORMTest\ORM\Laravel\ORM($this->log);
-        new ORMTest\ORM\Doctrine2\ORM($this->log);
-    }
+        protected function setUp()
+        {
+            $this->log = new Logger('orm-log');
+            $this->log->pushHandler(new StreamHandler(__DIR__ . '/../log.txt'));
 
-    /**
-     * @return array[ORMTest\Base\ORM]
-     */
-    protected function getORMs()
-    {
-        return Registry::getMany(Registry::types());
+            new ORMTest\ORM\Laravel\ORM($this->log);
+            new ORMTest\ORM\Doctrine2\ORM($this->log);
+        }
+
+        /**
+         * @return array[ORMTest\Base\ORM]
+         */
+        protected function getORMs()
+        {
+            return Registry::getMany(Registry::types());
+        }
     }
-}
